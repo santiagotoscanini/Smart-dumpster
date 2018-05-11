e4404f1d066cff9dbafd2903a394811e15679e12
Nicolás Marino <nicolas.marino.12@live.com>
2018-05-11 19:26:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Smart-garbage-dumpster
---
+++ index.js
@@ -0,0 +1,22 @@
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'es-UY';
+
+var btn = document.getElementById('btn1');
+var texto = document.createElement('p');
+var container = document.getElementById('textbox');
+
+
+btn.addEventListener('click', () => {
+    dictate();
+});
+
+const dictate = () => {
+    recognition.start();
+    recognition.onresult = (event) => {
+        const speechToText = event.results[0][0].transcript;
+        texto.textContent  = speechToText;
+        container.value = (texto.innerHTML);
+    }
+}
+
