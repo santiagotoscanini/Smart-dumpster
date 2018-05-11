cda39b5431b1089c03f4d58c2e48ccf46567891f
Nicolás Marino <nicolas.marino.12@live.com>
2018-05-11 19:52:39 -0300

    alot

--- README.md
+++
@@ -1,1 +0,0 @@
-# Smart-garbage-dumpster
--- index.js
+++ index.js
@@ -19,4 +19,20 @@ const dictate = () => {
         container.value = (texto.innerHTML);
     }
 }
-
+/*
+const stitch = require("mongodb-stitch")
+const clientPromise = stitch.StitchClientFactory.create('smartgarbagedumpster-wxlos');
+clientPromise.then(client => {
+  const db = client.service('mongodb', 'mongodb-atlas').db('sgd');
+  client.login().then(() =>
+    db.collection('tipos').updateOne({owner_id: client.authedId()}, {$set:{number:42}}, {upsert:true})
+  ).then(() =>
+    db.collection('tipos').find({owner_id: client.authedId()}).limit(100).execute()
+  ).then(docs => {
+    console.log("Found docs", docs)
+    console.log("[MongoDB Stitch] Connected to Stitch")
+  }).catch(err => {
+    console.error(err)
+  });
+});
+*/
