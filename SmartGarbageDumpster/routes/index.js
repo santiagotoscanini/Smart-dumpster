38a8834962c1c567cbac029c789b18d9271e13d2
Nicolás Marino <nicolas.marino.12@live.com>
2018-05-12 16:01:27 -0300

    Trying another thing

--- SmartGarbageDumpster/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* html */
-exports.index = function(req, res){
-  res.render('index', { title: 'Page title' });};
-
-
-module.exports = router;
